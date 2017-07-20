d22aedcb5221e272e0d15cf8e4e280ffb3a245c6
user_reg@qq.com <Yanbonnie308@hotmail.com>
2017-07-20 18:22:18 +0800

    提交

---
+++ src/view/login/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <!-- <img src="./assets/logo.png"> -->
+    <h3>我是登录页</h3>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style>
+
+</style>
